bff744284173627215b33a9b1ee8bcda90a6476c
Frank van den Boogaard <74182536+FrankvandenBoogaard@users.noreply.github.com>
2022-01-17 04:54:32 +0100

    competence smaller

--- src/panels/freelancers/PanelFreelancerProfile.jsx
+++ src/panels/freelancers/PanelFreelancerProfile.jsx
@@ -483,40 +483,7 @@ export default function PanelFreelancerProfile() {
                 </p>
               </div>
 
-              <div className='col-span-6 sm:col-span-4'>
-                <label
-                  htmlFor='image-url'
-                  className='block text-sm font-medium text-gray-700'
-                >
-                  LinkedIn image-URL
-                </label>
-                <div className='mt-1 relative'>
-                  <input
-                    type='text'
-                    {...register('imageUrl')}
-                    defaultValue={data?.freelancer.data.attributes.imageUrl}
-                    className={classNames(
-                      errors.imageUrl
-                        ? 'border-red-300 text-red-900 focus:outline-none focus:ring-red-500 focus:border-red-500'
-                        : 'shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300',
-                      'block w-full sm:text-sm rounded-md'
-                    )}
-                  />
-                  {errors.imageUrl && (
-                    <div className='absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none'>
-                      <ExclamationCircleIcon
-                        className='h-5 w-5 text-red-500'
-                        aria-hidden='true'
-                      />
-                    </div>
-                  )}
-                </div>
-                <p className='mt-2 text-sm text-red-600' id='imageUrl-error'>
-                  {errors.imageUrl?.message}
-                </p>
-              </div>
-
-              <div className='col-span-6 sm:col-span-4'>
+              <div className='col-span-6 sm:col-span-2'>
                 <label
                   htmlFor='competence'
                   className='block text-sm font-medium text-gray-700'
@@ -566,6 +533,41 @@ export default function PanelFreelancerProfile() {
                   {errors.competence?.message}
                 </p>
               </div>
+
+              <div className='col-span-6 sm:col-span-4'>
+                <label
+                  htmlFor='image-url'
+                  className='block text-sm font-medium text-gray-700'
+                >
+                  LinkedIn image-URL
+                </label>
+                <div className='mt-1 relative'>
+                  <input
+                    type='text'
+                    {...register('imageUrl')}
+                    defaultValue={data?.freelancer.data.attributes.imageUrl}
+                    className={classNames(
+                      errors.imageUrl
+                        ? 'border-red-300 text-red-900 focus:outline-none focus:ring-red-500 focus:border-red-500'
+                        : 'shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300',
+                      'block w-full sm:text-sm rounded-md'
+                    )}
+                  />
+                  {errors.imageUrl && (
+                    <div className='absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none'>
+                      <ExclamationCircleIcon
+                        className='h-5 w-5 text-red-500'
+                        aria-hidden='true'
+                      />
+                    </div>
+                  )}
+                </div>
+                <p className='mt-2 text-sm text-red-600' id='imageUrl-error'>
+                  {errors.imageUrl?.message}
+                </p>
+              </div>
+
+              
 
               <div className='col-span-6'>
                 <label
