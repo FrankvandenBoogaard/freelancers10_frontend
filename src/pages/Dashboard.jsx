3d66f104856298aa48c8f51778d02ce66142f2d6
Frank van den Boogaard <74182536+FrankvandenBoogaard@users.noreply.github.com>
2022-02-10 13:25:45 +0100

    dashboard purple to indigo

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -103,7 +103,7 @@ export default function Dashboard() {
           <select
             id='tabs'
             name='tabs'
-            className='mt-4 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-purple-500 focus:border-purple-500 sm:text-sm rounded-md'
+            className='mt-4 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md'
             defaultValue={tabs.find((tab) => tab.current).name}
           >
             {tabs.map((tab) => (
@@ -121,7 +121,7 @@ export default function Dashboard() {
                   href={tab.href}
                   className={classNames(
                     tab.current
-                      ? 'border-purple-500 text-purple-600'
+                      ? 'border-indigo-500 text-indigo-600'
                       : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200',
                     'whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm'
                   )}
@@ -131,7 +131,7 @@ export default function Dashboard() {
                     <span
                       className={classNames(
                         tab.current
-                          ? 'bg-purple-100 text-purple-600'
+                          ? 'bg-indigo-100 text-indigo-600'
                           : 'bg-gray-100 text-gray-900',
                         'hidden ml-2 py-0.5 px-2.5 rounded-full text-xs font-medium md:inline-block'
                       )}
@@ -369,10 +369,10 @@ export default function Dashboard() {
           >
             1
           </a>
-          {/* Current: "border-purple-500 text-purple-600", Default: "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200" */}
+          {/* Current: "border-indigo-500 text-indigo-600", Default: "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200" */}
           <a
             href='#'
-            className='border-purple-500 text-purple-600 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium'
+            className='border-indigo-500 text-indigo-600 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium'
             aria-current='page'
           >
             2
